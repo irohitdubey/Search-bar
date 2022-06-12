8ac4900adc8c90b6837940978b2bf2ea41146c60
irohitdubey <ceo.rohitdubey@gmail.com>
2022-06-12 12:15:08 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import "./App.css";
+import JSONDATA from "./MOCK_DATA.json";
+
+function App() {
+  const [searchTerm, setSearchTerm] = useState("");
+  return (
+    <div className="App">
+      <input
+        type="text"
+        placeholder="Search..."
+        onChange={(event) => {
+          setSearchTerm(event.target.value);
+        }}
+      />
+      {JSONDATA.filter((val) => {
+        if (searchTerm == "") {
+          return val;
+        } else if (
+          val.first_name.toLowerCase().includes(searchTerm.toLowerCase())
+        ) {
+          return val;
+        }
+      }).map((val) => {
+        return (
+          <div>
+            <p>{val.first_name}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
+export default App;
